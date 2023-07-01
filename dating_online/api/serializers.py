69b36a401b6260d540b6bb4acf47ad22f9a1cb46
Andrey Balakin <usdocs@ya.ru>
2023-07-01 17:45:13 +0300

    upgrade serializers.py

--- dating_online/api/serializers.py
+++ dating_online/api/serializers.py
@@ -1,22 +1,9 @@
-import base64
-
 from django.contrib.auth.hashers import make_password
-from django.core.files.base import ContentFile
 from rest_framework import serializers
 from users.models import SEX, User
 
 
-class Base64ImageField(serializers.ImageField):
-    def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
-            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
-        return super().to_internal_value(data)
-
-
 class CreateClientSerializer(serializers.ModelSerializer):
-    profile_picture = Base64ImageField()
     sex = serializers.ChoiceField(choices=SEX)
     password = serializers.CharField(write_only=True)
 
