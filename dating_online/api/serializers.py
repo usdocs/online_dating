ad0da1e5ffa5c5774a70f43d0f81f30f1acd8e23
Andrey Balakin <usdocs@ya.ru>
2023-07-15 17:29:37 +0300

    update views.py and serializers.py

--- dating_online/api/serializers.py
+++ dating_online/api/serializers.py
@@ -1,7 +1,11 @@
-from django.contrib.auth.hashers import make_password
+from api.include.send_mail import send_mail_match
+from api.include.watermark import watermark_with_transparency
+from django.conf import settings
+from django.contrib.auth.hashers import check_password, make_password
 from geopy.distance import great_circle
-from rest_framework import serializers
-from users.models import SEX, Coordinates, User
+from rest_framework import serializers, status
+from rest_framework.authtoken.models import Token
+from users.models import SEX, Coordinates, Match, User
 
 
 class CreateClientSerializer(serializers.ModelSerializer):
@@ -15,13 +19,45 @@ class CreateClientSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         validated_data['password'] = make_password(validated_data['password'])
+        validated_data['profile_picture'] = watermark_with_transparency(
+            validated_data['profile_picture'],
+            settings.WATERMARK_URL,
+            position=settings.WATERMARK_POSITION
+        )
         return super(CreateClientSerializer, self).create(validated_data)
 
 
 class TokenSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
         fields = ('email', 'password')
+        read_only_fields = ('email', 'password')
+
+    def to_internal_value(self, data):
+        return data
+
+    def validate(self, attrs):
+        user = User.objects.filter(email=attrs['email']).first()
+        if not user:
+            raise serializers.ValidationError(
+                'Пользователя с таким email не существует'
+            )
+        if not check_password(attrs['password'], user.password):
+            raise serializers.ValidationError(
+                'Введен неверный пароль'
+            )
+        return attrs
+
+    def create(self, validated_data):
+        token, created = Token.objects.get_or_create(
+            user=User.objects.filter(email=validated_data['email']).first()
+        )
+        if created:
+            stat = status.HTTP_201_CREATED
+        else:
+            stat = status.HTTP_200_OK
+        return {'response': {'auth_token': str(token)}, 'status': stat}
 
 
 class RetrieveClientSerializer(serializers.ModelSerializer):
@@ -29,7 +65,7 @@ class RetrieveClientSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('profile_picture', 'sex', 'first_name', 'last_name', 'email',
+        fields = ('profile_picture', 'sex', 'first_name', 'last_name',
                   'username', 'distance')
 
     def get_distance(self, obj):
@@ -52,3 +88,43 @@ class CoordinatesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Coordinates
         fields = ('longitude', 'latitude')
+
+    def create(self, validated_data):
+        coordinates = Coordinates.objects.filter(
+            user=self.context.get('request').user
+        ).first()
+        if coordinates:
+            coordinates.delete()
+        return super(CoordinatesSerializer, self).create(validated_data)
+
+
+class MatchClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('profile_picture', 'sex', 'first_name', 'last_name',
+                  'username')
+        read_only_fields = ('profile_picture', 'sex', 'first_name',
+                            'last_name', 'username')
+
+    def validate(self, attrs):
+        if self.context['request'].user == self.instance:
+            raise serializers.ValidationError(
+                'Вы не можете проявить симпатию к себе)'
+            )
+        return attrs
+
+    def update(self, instance, validated_data):
+        user = self.context['request'].user
+        match, created = Match.objects.get_or_create(
+            user=user,
+            liking=instance
+        )
+        if created:
+            raise serializers.ValidationError(
+                'Вы уже проявили симпатию к данному пользователю'
+            )
+        if instance.liker.filter(liking=user).first():
+            send_mail_match(user, instance)
+            send_mail_match(instance, user)
+            return {'email': instance.email}
+        return MatchClientSerializer(instance).data
--- dating_online/api/views.py
+++ dating_online/api/views.py
@@ -1,11 +1,7 @@
 from api.include.filters import DistanceFilter
-from api.include.send_mail import send_mail_match
-from api.include.watermark import watermark_with_transparency
 from api.serializers import (CoordinatesSerializer, CreateClientSerializer,
-                             RetrieveClientSerializer, TokenSerializer)
-from django.conf import settings
-from django.contrib.auth.hashers import check_password
-from django.shortcuts import get_object_or_404
+                             MatchClientSerializer, RetrieveClientSerializer,
+                             TokenSerializer)
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins, status
 from rest_framework.authtoken.models import Token
@@ -13,24 +9,13 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
-from users.models import Coordinates, Match, User
+from users.models import User
 
 
 class CreateClientViewSet(mixins.CreateModelMixin,
                           GenericViewSet):
     """Регистрация пользователя"""
     serializer_class = CreateClientSerializer
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        profile_picture_with_watermark = watermark_with_transparency(
-            request.data['profile_picture'],
-            settings.WATERMARK_URL,
-            position=settings.WATERMARK_POSITION
-        )
-        serializer.save(profile_picture=profile_picture_with_watermark)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     @action(
         detail=False,
@@ -40,9 +25,6 @@ class CreateClientViewSet(mixins.CreateModelMixin,
     )
     def coordinates(self, request):
         """Фиксирует текущие координаты"""
-        coordinates = Coordinates.objects.filter(user=request.user).first()
-        if coordinates:
-            coordinates.delete()
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save(user=request.user)
@@ -59,24 +41,13 @@ class AuthClientViewSet(GenericViewSet):
     )
     def login(self, request):
         """Выдает токен авторизации по емейлу и паролю"""
-        user = User.objects.filter(email=request.data['email']).first()
-        if not user:
-            return Response(
-                {'Пользователя с таким email не существует'},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        if not check_password(request.data['password'], user.password):
-            return Response(
-                {'Введен неверный пароль'},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        token, created = Token.objects.get_or_create(user=user)
-        if created:
-            stat = status.HTTP_201_CREATED
-        else:
-            stat = status.HTTP_200_OK
-        response = {'auth_token': str(token)}
-        return Response(response, status=stat)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            serializer.instance['response'],
+            status=serializer.instance['status']
+        )
 
     @action(
         detail=False,
@@ -93,7 +64,7 @@ class RetrieveMatchClientViewSet(mixins.RetrieveModelMixin,
                                  GenericViewSet):
     """Получение данных пользователя"""
     queryset = User.objects.all()
-    serializer_class = RetrieveClientSerializer
+    serializer_class = MatchClientSerializer
 
     @action(
         detail=True,
@@ -102,27 +73,11 @@ class RetrieveMatchClientViewSet(mixins.RetrieveModelMixin,
     )
     def match(self, request, pk):
         """Эндпоинт оценивания участником, только для авторизованных!"""
-        liking_user = get_object_or_404(User, id=pk)
-        user = get_object_or_404(User, username=self.request.user.username)
-        if str(user.id) == pk:
-            return Response(
-                {'Вы не можете проявить симпатию к себе)'},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        match, created = Match.objects.get_or_create(
-            user=user,
-            liking=liking_user
-        )
-        if not created:
-            return Response(
-                {'Вы уже проявили симпатию к данному пользователю'},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        serializer = self.get_serializer(liking_user)
-        if liking_user.liker.filter(liking=user).first():
-            send_mail_match(user, liking_user)
-            send_mail_match(liking_user, user)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.instance, status=status.HTTP_201_CREATED)
 
 
 class ListClientViewSet(mixins.ListModelMixin,
